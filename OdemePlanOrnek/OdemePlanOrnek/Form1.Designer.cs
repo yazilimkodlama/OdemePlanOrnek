b79b15c1c5966ef49e77ae083541695d5f62ee1f
yazilimkodlama <145126274+yazilimkodlama@users.noreply.github.com>
2024-12-19 13:48:11 +0300

    Add files via upload

---
+++ OdemePlanOrnek/OdemePlanOrnek/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace OdemePlanOrnek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTaksit = new System.Windows.Forms.ComboBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblToplamTutar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürün Adı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(117, 81);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(210, 26);
+            this.txtFiyat.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ürün Fiyatı";
+            // 
+            // cmbTaksit
+            // 
+            this.cmbTaksit.FormattingEnabled = true;
+            this.cmbTaksit.Location = new System.Drawing.Point(117, 113);
+            this.cmbTaksit.Name = "cmbTaksit";
+            this.cmbTaksit.Size = new System.Drawing.Size(210, 28);
+            this.cmbTaksit.TabIndex = 4;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(117, 147);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(210, 57);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(354, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(367, 504);
+            this.listBox1.TabIndex = 6;
+            // 
+            // lblToplamTutar
+            // 
+            this.lblToplamTutar.AutoSize = true;
+            this.lblToplamTutar.Location = new System.Drawing.Point(12, 524);
+            this.lblToplamTutar.Name = "lblToplamTutar";
+            this.lblToplamTutar.Size = new System.Drawing.Size(187, 20);
+            this.lblToplamTutar.TabIndex = 7;
+            this.lblToplamTutar.Text = "Toplam Ödenecek Tutar: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 567);
+            this.Controls.Add(this.lblToplamTutar);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.cmbTaksit);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTaksit;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblToplamTutar;
+    }
+}
+
---
+++ OdemePlanOrnek/OdemePlanOrnek/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OdemePlanOrnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbTaksit.Items.Add("Peşin");
+            for (int i = 2; i <= 12; i++)
+            {
+                cmbTaksit.Items.Add(i + " Taksit");
+            }
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            double fiyat = 0;
+            DateTime tarih= DateTime.Now;
+            fiyat=Convert.ToDouble(txtFiyat.Text);
+            int taksitSayisi=cmbTaksit.SelectedIndex+1;
+
+            if (taksitSayisi >= 6 && taksitSayisi<=9)
+            {
+                fiyat = fiyat * 1.1;
+            }
+            else if (taksitSayisi > 9 && taksitSayisi<=12)
+            {
+                fiyat = fiyat * 1.2;
+            }
+            else
+            {
+                fiyat = fiyat;
+            }
+
+            double taksitTutar = fiyat / taksitSayisi;
+            //MessageBox.Show(taksitTutar + "");
+
+            for(int i = 1; i <= taksitSayisi; i++)
+            {
+                tarih=tarih.AddMonths(1);
+                listBox1.Items.Add(i+".Taksit:"+tarih.ToShortDateString()+"   Ödenecek Tutar:"+Math.Round(taksitTutar,2) + " ₺");
+
+            }
+
+            lblToplamTutar.Text = "Toplam Ödenecek Tutar: " + fiyat + "₺";
+
+
+        }
+    }
+}
